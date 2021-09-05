5e9f0c5da0c8e0e30207496a78c2ee16fe3b2aaf
ButerD <74465124+ButerD@users.noreply.github.com>
2021-09-05 14:27:25 +0300

    add task 2.1.3

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,6 +1,9 @@
 package app.model;
 
-public class Timer {
+import org.springframework.stereotype.Component;
+
+@Component("getTimer")
+public class Timer extends Animal {
 
     private Long nanoTime = System.nanoTime();
 
